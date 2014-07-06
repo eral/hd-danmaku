6041ad438df722e43bb03da6738f2c60f9223699
eral.r.l <eral.r.l@gmail.com>
2014-07-07 05:16:24 +0900

    DestroyOnBecameInvisible:画面外破棄の実装

---
+++ Assets/Utility/DestroyOnBecameInvisible/DestroyOnBecameInvisible.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnBecameInvisible : MonoBehaviour {
+
+	/// <summary>
+	/// Raises the became invisible event.
+	/// </summary>
+	void OnBecameInvisible(){
+		Destroy(this.gameObject);
+	}
+}
