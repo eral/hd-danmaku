c5e75bd858e90f2bf279a04eaf9ed0703a98737d
eral.r.l <eral.r.l+github.com@gmail.com>
2014-08-26 00:20:46 +0900

    FpsText:Optimize to UI.Text of Unity4.6
    UI.Text用に最適化
    
    Conflicts:
    	Assets/Utility/FpsText/FpsText.cs

--- Assets/Utility/FpsText/FpsText.cs
+++ Assets/Utility/FpsText/FpsText.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 
-[RequireComponent(typeof(GUIText))]
+[RequireComponent(typeof(UnityEngine.UI.Text))]
 public class FpsText : MonoBehaviour {
-	public GUIText	m_Target;
-	public string	m_Format = "{0: 00.00}<size=10>fps</size>";
-	public float[]	m_Times;
-	public int		m_Current;
-	public bool		m_RealtimeBase = false;
+	public UnityEngine.UI.Text	m_Target;
+	public string				m_Format = "{0: 00.00}<size=10>fps</size>";
+	public float[]				m_Times;
+	public int					m_Current;
+	public bool					m_RealtimeBase = false;
 
 	private float	m_LastRealtime;
 
@@ -17,7 +17,7 @@ public class FpsText : MonoBehaviour {
 	/// </summary>
 	void Start () {
 		if (!m_Target) {
-			m_Target = GetComponent<GUIText>();
+			m_Target = GetComponent<UnityEngine.UI.Text>();
 		}
 		if (0 == m_Times.Length) {
 			m_Times = new float[6];
