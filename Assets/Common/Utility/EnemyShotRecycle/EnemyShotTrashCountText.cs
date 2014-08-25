ea6f45fe45ff03d092abbc0970b93b66be74354a
eral.r.l <eral.r.l+github.com@gmail.com>
2014-08-26 00:27:12 +0900

    EnemyShotTrashCountText:enable to work
    EnemyShotTrashタブのオブジェクトの除去に伴い動かなくなっていた不具合の修正

--- Assets/Common/Utility/EnemyShotRecycle/EnemyShotTrashCountText.cs
+++ Assets/Common/Utility/EnemyShotRecycle/EnemyShotTrashCountText.cs
@@ -8,8 +8,6 @@ public class EnemyShotTrashCountText : MonoBehaviour {
 	public LayerMask	m_LayerTarget;
 	public string		m_Format = "{0: ###0}<size=10>trashes</size>";
 	
-	private Transform m_EnemyShotTrash = null;
-	
 	/// <summary>
 	/// 初回更新前
 	/// </summary>
@@ -17,18 +15,13 @@ public class EnemyShotTrashCountText : MonoBehaviour {
 		if (!m_Target) {
 			m_Target = GetComponent<GUIText>();
 		}
-		var enemy_shot_trash = GameObject.FindGameObjectWithTag("EnemyShotTrash");
-		if (enemy_shot_trash) {
-			m_EnemyShotTrash = enemy_shot_trash.transform;
-		}
 	}
 	
 	/// <summary>
 	/// 更新
 	/// </summary>
 	void Update () {
-		var count = m_EnemyShotTrash.childCount;
-		count += OrbitInstance.Instantiate().GetTrashCount();
+		var count = OrbitInstance.Instantiate().GetTrashCount();
 		m_Target.text = string.Format(m_Format, count);
 	}
 }
